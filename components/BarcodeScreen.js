1fe76cc06c17977b633cc381b383d7eef3d2de7b
jeffli <jeff.chun.yinli@gmail.com>
2018-12-30 18:08:47 -0500

    copied barcode scanner code from https://docs.expo.io/versions/v31.0.0/sdk/bar-code-scanner

--- components/BarcodeScreen.js
+++ components/BarcodeScreen.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import { BarCodeScanner, Permissions } from 'expo';
 
 
 class BarcodeScreen extends React.Component {
+  state = {
+    hasCameraPermission: null,
+  }
+
+  async componentDidMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
   render() {
+    const { hasCameraPermission } = this.state;
+
+    if (hasCameraPermission === null) {
+      return <Text>Requesting for camera permission</Text>;
+    }
+    if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    }
     return (
-      <View>
-        <Text>Placeholder screen for barcode scanner</Text>
+      <View style={{ flex: 1 }}>
+        <BarCodeScanner
+          onBarCodeScanned={this.handleBarCodeScanned}
+          style={StyleSheet.absoluteFill}
+        />
       </View>
-    )
+    );
+  }
+
+  handleBarCodeScanned = ({ type, data }) => {
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   }
 }
 
