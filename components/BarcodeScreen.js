2105f33c5fe9d4e09977452001e23f29b5399ec9
jeffli <jeff.chun.yinli@gmail.com>
2018-12-30 19:23:45 -0500

    added opening the yroo result url after a successful barcode scan + removed unused styes on home screen

--- components/BarcodeScreen.js
+++ components/BarcodeScreen.js
@@ -1,16 +1,42 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-import { BarCodeScanner, Permissions } from 'expo';
+import { BarCodeScanner, Permissions, WebBrowser } from 'expo';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default class BarcodeScreen extends React.Component {
   state = {
     hasCameraPermission: null,
+    scanned: false
   }
 
   async componentDidMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  renderBarCodeScanner = () => {
+    return (
+      <BarCodeScanner
+        onBarCodeScanned={this.handleBarCodeScanned}
+        style={StyleSheet.absoluteFill}
+      />
+    )
+  }
+
+  renderScannedScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Text>This is just the home screen, does nothing</Text>
+      </View>
+    )
   }
 
   render() {
@@ -24,15 +50,27 @@ export default class BarcodeScreen extends React.Component {
     }
     return (
       <View style={{ flex: 1 }}>
-        <BarCodeScanner
-          onBarCodeScanned={this.handleBarCodeScanned}
-          style={StyleSheet.absoluteFill}
-        />
+        {
+          this.state.scanned ? this.renderScannedScreen() : this.renderBarCodeScanner()
+        }
+
       </View>
     );
   }
 
-  handleBarCodeScanned = ({ type, data }) => {
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  handleBarCodeScanned = async ({ type, data }) => {
+    this.setState({ scanned: true });
+    alert('Looking for result');
+    let response = await fetch(`https://yroo.com/api/identifiers/upc/${data}`);
+    let json = await response.json();
+
+    let resultUrl = json.resultUrl
+    if (resultUrl) {
+      await WebBrowser.openBrowserAsync(resultUrl);
+    } else {
+      alert(`debug info: ${JSON.stringify(json)}. Nothing found with type ${type} and data ${data}!`);
+    }
+
+    this.setState({ scanned: false});
   }
 }
